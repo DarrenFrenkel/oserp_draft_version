55dbaee2fd20d124a35563338b542bdccbabbb7b
Darren Frenkel <darrenfrenkel@gmail.com>
2014-01-07 02:09:33 +0000

    updated views.py to reflect new models

--- erp_app/views.py
+++ erp_app/views.py
@@ -8,6 +8,6 @@ from erp_app.models import Customer
 from django.template import RequestContext, loader
 
 def index(request):
-    customer_info = Customers.objects.all()
+    customer_info = Customer.objects.all()
     context = RequestContext(request, {'customer_info': customer_info,})
     return render(request, 'erp_app/index.html', context)
